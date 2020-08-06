806ffe80a0fc475d4766d9d8fc60f1004cfb3d29
Irfan Zainudin <m.irfan.zain@gmail.com>
2020-08-06 21:07:39 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const myname = document.getElementById('my-name');
+const uni = document.getElementById('university');
+const projects = document.getElementById('projects');
+
+myname.innerText = 'Irfan Zainudin';
+uni.innerText = 'Software Engineering @ UNSW';
+projects.innerText = 'Projects';
