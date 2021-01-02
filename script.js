f3a1f98f971855b1708d85cc9120dc5d80c53d5c
Irfan Zainudin <m.irfan.zain@gmail.com>
2021-01-02 18:35:14 +1100

    made it responsive

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const myname = document.getElementById('my-name');
-const uni = document.getElementById('university');
-const projects = document.getElementById('projects');
+const root = document.getElementById('root');
 
-myname.innerText = 'Irfan Zainudin';
+const myName = document.getElementById('my-name');
+const uni = document.getElementById('university');
+
+myName.innerText = 'Irfan Zainudin';
 uni.innerText = 'Software Engineering @ UNSW';
-projects.innerText = 'Projects';
+
+left = () => {
+    const webdevProjects = document.getElementById('webdev-projects');
+    const gamedevProjects = document.getElementById('gamedev-projects');
+
+    if (webdevProjects.style.display.match('none')) {
+        webdevProjects.setAttribute('style', 'display: block');
+        gamedevProjects.setAttribute('style', 'display: none');
+    }
+}
+
+right = () => {
+    const webdevProjects = document.getElementById('webdev-projects');
+    const gamedevProjects = document.getElementById('gamedev-projects');
+
+    if (gamedevProjects.style.display.match('none')) {
+        webdevProjects.setAttribute('style', 'display: none');
+        gamedevProjects.setAttribute('style', 'display: block');
+    }
+}
