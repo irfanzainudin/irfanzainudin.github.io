f4834c36ec6a7ec0af84fb90079a4e95865c8b9f
irfanzainudin <m.irfan.zain@gmail.com>
2021-08-24 13:05:49 +1000

    using react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const pages = [
+  "Home"
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Irfan Zainudin</h1>
+    </div>
+  );
+}
+
+export default App;
